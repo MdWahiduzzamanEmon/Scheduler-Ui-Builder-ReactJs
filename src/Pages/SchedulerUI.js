a5239069fe26a8cd415878d1a4249ffff6aa20de
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2022-01-23 01:19:17 +0600

    design done

--- src/Pages/SchedulerUI.js
+++ src/Pages/SchedulerUI.js
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable prettier/prettier */
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
@@ -9,53 +7,48 @@ import {
   Appointments,
   DragDropProvider,
   EditRecurrenceMenu,
-    AllDayPanel,
-    AppointmentTooltip,
-  AppointmentForm 
+  AllDayPanel,
+  AppointmentTooltip,
+  AppointmentForm
 } from '@devexpress/dx-react-scheduler-material-ui';
 import appointmentComponent from './AppointMentComponent';
-import {allowDrag} from './AppointMentComponent';
-import {recurrenceAppointment} from './Data';
+import { allowDrag } from './AppointMentComponent';
+import { recurrenceAppointment } from './Data';
 const SchedulerUi = () => {
-   
-    const currentDate = new Date('2018-06-27');
-    
-    const [recurrenceAppointments, setRecurrenceAppointments] =
-      React.useState(recurrenceAppointment);
-   
+  const currentDate = new Date('2018-06-27');
 
-    const onCommitChanges = ({ added, changed, deleted }) => {
+  const [recurrenceAppointments, setRecurrenceAppointments] = React.useState(recurrenceAppointment);
 
-      let appointments = [...recurrenceAppointments];
-      if (added) {
-        appointments = appointments.concat(added);
-      }
-      if (changed) {
-        appointments = appointments.map((appointment) =>
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
-        );
-      }
-      if (deleted) {
-        appointments = appointments.filter((appointment) => appointment.id !== deleted);
-      }
-      setRecurrenceAppointments(appointments);
-    };
-    
-     return (
-       <Paper>
-         <Scheduler data={recurrenceAppointments} height={450}>
-           <ViewState defaultCurrentDate={currentDate} />
-           <EditingState onCommitChanges={onCommitChanges} />
-           <EditRecurrenceMenu />
-           <WeekView startDayHour={9} endDayHour={16} />
-           <Appointments appointmentComponent={appointmentComponent} />
-           <AllDayPanel />
-           <DragDropProvider allowDrag={allowDrag} />
-           <AppointmentTooltip showOpenButton showDeleteButton />
-           <AppointmentForm />
-         </Scheduler>
-       </Paper>
-     );
+  const onCommitChanges = ({ added, changed, deleted }) => {
+    let appointments = [...recurrenceAppointments];
+    if (added) {
+      appointments = appointments.concat(added);
     }
-export default SchedulerUi;
+    if (changed) {
+      appointments = appointments.map((appointment) =>
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
+      );
+    }
+    if (deleted) {
+      appointments = appointments.filter((appointment) => appointment.id !== deleted);
+    }
+    setRecurrenceAppointments(appointments);
+  };
 
+  return (
+    <Paper>
+      <Scheduler data={recurrenceAppointments} height={500}>
+        <ViewState defaultCurrentDate={currentDate} />
+        <EditingState onCommitChanges={onCommitChanges} />
+        <EditRecurrenceMenu />
+        <WeekView startDayHour={9} endDayHour={16} />
+        <Appointments appointmentComponent={appointmentComponent} />
+        <AllDayPanel />
+        <DragDropProvider allowDrag={allowDrag} />
+        <AppointmentTooltip showOpenButton showDeleteButton />
+        <AppointmentForm />
+      </Scheduler>
+    </Paper>
+  );
+};
+export default SchedulerUi;
